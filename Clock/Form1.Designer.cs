20d0cb696134d7d1619ce3a24fceb658baf1d41d
Ryabchenko Andrey <xahtep19977@gmail.com>
2021-06-14 17:31:50 +0300

    adding files

---
+++ Clock/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.applicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.widthImagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectImageBackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectImageClockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extraClockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ownDateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.visibleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.applicationToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(414, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // applicationToolStripMenuItem
+            // 
+            this.applicationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem});
+            this.applicationToolStripMenuItem.Name = "applicationToolStripMenuItem";
+            this.applicationToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.applicationToolStripMenuItem.Text = "Приложение";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitToolStripMenuItem.Text = "Выйти";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.imagesToolStripMenuItem,
+            this.extraClockToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.viewToolStripMenuItem.Text = "Вид";
+            // 
+            // imagesToolStripMenuItem
+            // 
+            this.imagesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.widthImagesToolStripMenuItem,
+            this.selectImageBackToolStripMenuItem,
+            this.selectImageClockToolStripMenuItem});
+            this.imagesToolStripMenuItem.Name = "imagesToolStripMenuItem";
+            this.imagesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.imagesToolStripMenuItem.Text = "Картинки";
+            // 
+            // widthImagesToolStripMenuItem
+            // 
+            this.widthImagesToolStripMenuItem.Name = "widthImagesToolStripMenuItem";
+            this.widthImagesToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.widthImagesToolStripMenuItem.Text = "Отображение";
+            this.widthImagesToolStripMenuItem.Click += new System.EventHandler(this.widthImagesToolStripMenuItem_Click);
+            // 
+            // selectImageBackToolStripMenuItem
+            // 
+            this.selectImageBackToolStripMenuItem.Name = "selectImageBackToolStripMenuItem";
+            this.selectImageBackToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.selectImageBackToolStripMenuItem.Text = "Картинка для фона...";
+            this.selectImageBackToolStripMenuItem.Click += new System.EventHandler(this.selectImageBackToolStripMenuItem_Click);
+            // 
+            // selectImageClockToolStripMenuItem
+            // 
+            this.selectImageClockToolStripMenuItem.Name = "selectImageClockToolStripMenuItem";
+            this.selectImageClockToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.selectImageClockToolStripMenuItem.Text = "Картинка для часов...";
+            this.selectImageClockToolStripMenuItem.Click += new System.EventHandler(this.selectImageClockToolStripMenuItem_Click);
+            // 
+            // extraClockToolStripMenuItem
+            // 
+            this.extraClockToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.visibleToolStripMenuItem,
+            this.ownDateToolStripMenuItem});
+            this.extraClockToolStripMenuItem.Name = "extraClockToolStripMenuItem";
+            this.extraClockToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.extraClockToolStripMenuItem.Text = "Доп.часы";
+            // 
+            // ownDateToolStripMenuItem
+            // 
+            this.ownDateToolStripMenuItem.Name = "ownDateToolStripMenuItem";
+            this.ownDateToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.ownDateToolStripMenuItem.Text = "Своя дата";
+            this.ownDateToolStripMenuItem.Click += new System.EventHandler(this.ownDateToolStripMenuItem_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "d.M.y HH:mm";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 27);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(109, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.Visible = false;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(302, 249);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(100, 100);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.ForeColor = System.Drawing.Color.Aquamarine;
+            this.label1.Location = new System.Drawing.Point(3, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // visibleToolStripMenuItem
+            // 
+            this.visibleToolStripMenuItem.Checked = true;
+            this.visibleToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.visibleToolStripMenuItem.Name = "visibleToolStripMenuItem";
+            this.visibleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.visibleToolStripMenuItem.Text = "Отображение";
+            this.visibleToolStripMenuItem.Click += new System.EventHandler(this.visibleToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(414, 361);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(430, 400);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem applicationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imagesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem widthImagesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectImageBackToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectImageClockToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem extraClockToolStripMenuItem;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ToolStripMenuItem ownDateToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem visibleToolStripMenuItem;
+    }
+}
+
---
+++ Clock/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+        bool widthImages = false;
+        int size, sizeExtra;
+        DateTime needDate;
+        Image imgClock = Image.FromFile("clock.jpg");
+        Image imgBack = Image.FromFile("back.jpg");
+        DateTime dateNewYear = new DateTime(2017, 1, 1, 00, 00, 00);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dateTimePicker1.Value = dateNewYear;
+
+            Timer timerRefresh = new Timer();
+            timerRefresh.Interval = 1000;
+            timerRefresh.Tick += Timer_Tick;
+            timerRefresh.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            size = Math.Min(ClientRectangle.Width, ClientRectangle.Height) / 2 - 70;
+
+            if (widthImages)
+            {
+                Bitmap bmp = new Bitmap(imgBack, Size);
+                BackgroundImage = bmp;
+            }
+            else
+            {
+                BackgroundImage = null;
+            }
+
+            PaintClock(e.Graphics);
+            ShowTime(e.Graphics);
+
+            panel1.Invalidate();
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            sizeExtra = Math.Min(panel1.Width, panel1.Height) / 2 - 10;
+            if (widthImages)
+            {
+                Bitmap bmp = new Bitmap(imgBack, panel1.Size);
+                panel1.BackgroundImage = bmp;
+            }
+            else
+            {
+                panel1.BackgroundImage = null;
+            }
+            PaintExtraClock(e.Graphics);
+            ShowExtraTime(e.Graphics);
+        }
+
+        private void ShowExtraTime(Graphics g)
+        {
+
+            Pen arrowHour = new Pen(Color.DarkTurquoise, 3);
+            Pen arrowMinute = new Pen(Color.Turquoise, 2);
+
+            arrowHour.StartCap = LineCap.Round;
+            arrowMinute.StartCap = LineCap.Round;
+            arrowHour.EndCap = LineCap.ArrowAnchor;
+            arrowMinute.EndCap = LineCap.ArrowAnchor;
+            TimeSpan difference = needDate - DateTime.Now;
+            float angle = (difference.Hours > 12 ? difference.Hours - 12 : difference.Hours) * 30;
+
+            g.RotateTransform(angle);
+            g.DrawLine(arrowHour, 0, 0, sizeExtra / 2, 0);
+            g.RotateTransform(-angle);
+
+            angle = difference.Minutes * 6;
+            g.RotateTransform(angle);
+            g.DrawLine(arrowMinute, 0, 0, (int)(sizeExtra / 1.5), 0);
+            g.RotateTransform(-angle);
+
+            label1.Text = difference.Days > 0 ? $"Осталось дней: {difference.Days}" : "";
+        }
+
+        private void PaintExtraClock(Graphics g)
+        {
+            Matrix m = g.Transform;
+            Rectangle rect = new Rectangle(-sizeExtra, -sizeExtra, sizeExtra * 2, sizeExtra * 2);
+            m.Translate(panel1.Width / 2f, panel1.Height / 2f);
+            m.Rotate(-90);
+            g.Transform = m;
+
+            if (widthImages)
+            {
+                Bitmap bmp = new Bitmap(imgClock, rect.Size);
+                TextureBrush brush = new TextureBrush(bmp);
+                brush.TranslateTransform(-sizeExtra, -sizeExtra);
+                brush.RotateTransform(90);
+                g.FillEllipse(brush, rect);
+            }
+            else
+            {
+                g.FillEllipse(Brushes.Azure, rect);
+            }
+
+            Pen pentStrokes = new Pen(Color.MediumTurquoise);
+            pentStrokes.EndCap = LineCap.Round;
+            pentStrokes.StartCap = LineCap.Round;
+
+            for (int i = 0; i < 360; i += 30)
+            {
+                if (i % 30 == 0)
+                {
+                    int x = sizeExtra;
+                    pentStrokes.Width = 3;
+                    pentStrokes.Color = Color.Turquoise;
+                    g.DrawLine(pentStrokes, x - 1, 0, x + 1, 0);
+
+                }
+                g.RotateTransform(30);
+            }
+        }
+
+        private void ShowTime(Graphics g)
+        {
+
+            Pen arrowHour = new Pen(Color.PaleTurquoise, 5);
+            Pen arrowMinute = new Pen(Color.PaleTurquoise, 3);
+            Pen arrowSecond = new Pen(Color.Turquoise, 2);
+
+            arrowHour.StartCap = LineCap.Round;
+            arrowMinute.StartCap = LineCap.Round;
+            arrowSecond.StartCap = LineCap.Round;
+            arrowHour.EndCap = LineCap.ArrowAnchor;
+            arrowMinute.EndCap = LineCap.ArrowAnchor;
+            arrowSecond.EndCap = LineCap.ArrowAnchor;
+
+            float angle = (DateTime.Now.Hour > 12 ? DateTime.Now.Hour - 12 : DateTime.Now.Hour) * 30;
+
+            g.RotateTransform(angle);
+            g.DrawLine(arrowHour, 0, 0, size / 2, 0);
+            g.RotateTransform(-angle);
+
+            angle = DateTime.Now.Minute * 6;
+            g.RotateTransform(angle);
+            g.DrawLine(arrowMinute, 0, 0, (int)(size / 1.5), 0);
+            g.RotateTransform(-angle);
+
+            angle = DateTime.Now.Second * 6;
+            g.RotateTransform(angle);
+            g.DrawLine(arrowSecond, -20, 0, size, 0);
+            g.RotateTransform(-angle);
+        }
+
+        private void PaintClock(Graphics g)
+        {
+            Matrix m = g.Transform;
+            Rectangle rect = new Rectangle(-size, -size, size * 2, size * 2);
+            m.Translate(ClientRectangle.Width / 2f, ClientRectangle.Height / 2f);
+            m.Rotate(-90);
+            g.Transform = m;
+            if (widthImages)
+            {
+                Bitmap bmp = new Bitmap(imgClock, rect.Size);
+                TextureBrush brush = new TextureBrush(bmp);
+                brush.TranslateTransform(-size, -size);
+                brush.RotateTransform(90);
+                g.FillEllipse(brush, rect);
+            }
+            else
+            {
+                g.FillEllipse(Brushes.Azure, rect);
+            }
+            //g.DrawEllipse(Pens.Black, rect);
+            Pen pentStrokes = new Pen(Color.MediumTurquoise);
+            pentStrokes.EndCap = LineCap.Round;
+            pentStrokes.StartCap = LineCap.Round;
+
+            for (int i = 0; i < 360; i++)
+            {
+                if (i % 30 == 0)
+                {
+                    int x = size;
+                    pentStrokes.Width = 3;
+                    pentStrokes.Color = Color.PaleTurquoise;
+                    g.DrawLine(pentStrokes, x - 10, 0, x + 10, 0);
+                    g.DrawString((i == 0 ? 360 : i) / 30 + "", new Font("Courier", size / 12), Brushes.MediumTurquoise, x, -15, new StringFormat(StringFormatFlags.DirectionVertical));
+                }
+                else if (i % 6 == 0)
+                {
+                    pentStrokes.Width = 1;
+                    pentStrokes.Color = Color.Cyan;
+                    g.DrawLine(pentStrokes, size - 5, 0, size + 5, 0);
+                }
+                else
+                {
+                    pentStrokes.Width = 1;
+                    pentStrokes.Color = Color.LightGreen;
+                    g.DrawLine(pentStrokes, size - 2, 0, size + 2, 0);
+                }
+                g.RotateTransform(1);
+            }
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+            needDate = dateTimePicker1.Value;
+            Invalidate();
+        }
+
+        private void widthImagesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            widthImagesToolStripMenuItem.Checked = !widthImagesToolStripMenuItem.Checked;
+            widthImages = widthImagesToolStripMenuItem.Checked;
+            Invalidate();
+        }
+
+        private void visibleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            visibleToolStripMenuItem.Checked = !visibleToolStripMenuItem.Checked;
+            panel1.Visible = visibleToolStripMenuItem.Checked;
+        }
+
+        private void selectImageBackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = ("Картинки|*.bmp;*.jpg;*.jpeg;*.png");
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                imgBack = Image.FromFile(dlg.FileName);
+                Invalidate();
+            }
+        }
+
+        private void selectImageClockToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = ("Картинки|*.bmp;*.jpg;*.jpeg;*.png");
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                imgClock = Image.FromFile(dlg.FileName);
+                Invalidate();
+            }
+        }
+
+        private void ownDateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ownDateToolStripMenuItem.Checked = !ownDateToolStripMenuItem.Checked;
+            dateTimePicker1.Visible = ownDateToolStripMenuItem.Checked;
+            if (!dateTimePicker1.Visible)
+            {
+                dateTimePicker1.Value = dateNewYear;
+            }
+        }
+    }
+}
